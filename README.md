0c85e6fc327f481eb88f0238d28950b2f399ef2f
Морковин Александр Вячеславович <amorkovin@sportmaster.ru>
2023-09-24 12:38:29 +0300

    ДЗ1 Структура проекта

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ok-accounting
 
 Проект обучения на курсе [Kotlin Backend Developer. Professional](https://otus.ru/lessons/kotlin/)
+
 Тема: Система учета финансов
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "ok-accounting"
+include("m1l1-hello")
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion apply false
+    }
+}
